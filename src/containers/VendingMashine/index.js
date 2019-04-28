04b53306a5244c6ba986895f6571312437fe7e95
evroka <nata_safonova@inbox.ru>
2019-04-28 22:32:54 +0400

    feat(VendingMashine): add component ControlPanel
    Styles corrections are intended to render a new component properly

--- src/containers/VendingMashine/index.js
+++ src/containers/VendingMashine/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-
 import styles from './styles.module.scss'
+import ControlPanel from '../../containers/ControlPanel'
 
 
 class VendingMashine extends React.Component {
@@ -8,9 +8,14 @@ class VendingMashine extends React.Component {
 		return (
 			<div className={styles.wrap}>
 				<div className={styles.container}>
-					<div className={styles.standLeft}></div>
-					<div className={styles.standRight}></div>
+					<div className={styles.itemsContainer}>
+						<ControlPanel />
+					</div>
 				</div>
+				<div className={styles.stands}>
+						<div className={styles.standLeft}></div>
+						<div className={styles.standRight}></div>
+					</div>
 			</div>
 		)
 	}
