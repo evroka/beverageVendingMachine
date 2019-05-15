228c3ff6a19319a37e85c256c57b405f87879930
evroka <nata_safonova@inbox.ru>
2019-05-15 23:12:31 +0400

    feat(MoneyButtons): add styles for new component

--- src/components/MoneyButtons/index.js
+++ src/components/MoneyButtons/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import styles from './styles.module.scss'
 
-const MoneyButtons = ({ children }) => (
-	<div className={styles.container}>{children}</div>
+const MoneyButtons = () => (
+	<div className={styles.buttonsContainer}>
+		<div className={styles.oneRub}></div>
+		<div className={styles.twoRub}></div>
+		<div className={styles.fiveRub}></div>
+		<div className={styles.tenRub}></div>
+	</div>
 )
 
 export default MoneyButtons
